0a0764cac6c2c0e482633d492068873d36ae960e
EC2 Default User <yukimei0114@gmail.com>
2024-05-20 03:55:43 +0000

    [Add]create機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get  'top' => 'homes#top'
-  resources :books
+  get 'top' => 'homes#top'
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
